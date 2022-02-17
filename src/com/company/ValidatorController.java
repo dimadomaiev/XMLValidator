9e1016382a88795ab56ebf0a74390aa50614efd7
dima-domaiev@yandex.ru <Dinamit_5903218>
2022-02-17 19:11:10 +0200

    adding multiple choose file

--- src/com/company/ValidatorController.java
+++ src/com/company/ValidatorController.java
@@ -7,7 +7,6 @@ import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Text;
-import javafx.scene.text.TextFlow;
 import javafx.stage.Stage;
 import org.xml.sax.SAXException;
 
